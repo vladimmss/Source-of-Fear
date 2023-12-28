a304661a0f84275d1d6a871da4d01a4862d06b86
s1rexx <yo_s1rex@mail.ru>
2023-12-28 15:22:54 +0300

    Меню настроек (требуется доработка)

--- main_menu.py
+++ main_menu.py
@@ -2,6 +2,8 @@ import sys
 import os
 import pygame
 import random
+# import time
+from pygame.locals import *
 
 
 def load_image(name, colorkey=None):
@@ -21,12 +23,13 @@ def load_image(name, colorkey=None):
 
 
 class Button:
-    def __init__(self, x, y, wd, ht, image, text=None, sound=None, extra_im=None):
-        self.x, self.y = x, y
-        self.width, self.height = wd, ht
+    def __init__(self, pos: tuple, size: tuple, image=None, text=None, sound=None, extra_im=None):
+        self.x, self.y = pos
+        self.width, self.height = size
 
-        self.image = pygame.transform.scale(load_image(image), (self.width, self.height))
-        self.rect = self.image.get_rect(topleft=(x, y))
+        if image:
+            self.image = pygame.transform.scale(load_image(image), (self.width, self.height))
+            self.rect = self.image.get_rect(topleft=pos)
 
         if text:
             self.text = text
@@ -34,15 +37,13 @@ class Button:
             self.sound = pygame.mixer.Sound(sound)
             self.sound.set_volume(0.3)
         if extra_im:
-            self.extra_im = pygame.transform.scale(load_image(extra_im), (self.width, self.height))
+            self.extra_im = pygame.transform.scale(load_image(image), (self.width, self.height))
+
         self.intersection = False
 
     def draw(self, sc, mouse_pos):
-        button_image = self.extra_im if self.intersection else self.image
-        sc.blit(button_image, self.rect)
-
-        font = pygame.font.Font(None, 40)
-        text_rendered = font.render(self.text, 1, (200, 200, 200))
+        font = pygame.font.Font('data/MorfinSans-Regular.ttf', 70)
+        text_rendered = font.render(self.text, 1, (255, 255, 255))
         text_rect = text_rendered.get_rect(center=self.rect.center)
         sc.blit(text_rendered, text_rect)
 
@@ -51,81 +52,150 @@ class Button:
     def button_pressed(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and self.intersection:
             self.sound.play()
+            # time.sleep(0.3)
             pygame.event.post(pygame.event.Event(pygame.USEREVENT, button=self))
 
 
-pygame.init()
-size = width, height = 700, 500
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption('Source of Fear')
+class MainMenu:
+    def __init__(self):
+        pygame.init()
+        self.size = self.width, self.height = 1920, 1080
+        self.screen = pygame.display.set_mode(self.size, pygame.FULLSCREEN)
+        pygame.display.set_caption('Source of Fear')
 
-pygame.mixer.music.load('data/menu_music.mp3')
-pygame.mixer.music.set_volume(0.3)
-pygame.mixer.music.play(-1)
+        pygame.mixer.music.load('data/menu_music.mp3')
+        pygame.mixer.music.set_volume(0.3)
+        pygame.mixer.music.play(-1)
 
-clock = pygame.time.Clock()
+        self.clock = pygame.time.Clock()
 
+    def fill_background(self):
+        for i in range(3000):
+            self.screen.fill(random.choice(((51, 51, 51), (102, 102, 102))),
+                             (random.random() * self.width, random.random() * self.height, 7, 2))
 
-def fill_background():
-    for i in range(3000):
-        screen.fill(random.choice(((51, 51, 51), (102, 102, 102))),
-                    (random.random() * width, random.random() * height, 7, 2))
+    def main_window(self):
+        running = True
 
-
-def main_menu():
-    running = True
-
-    new_game_button = Button(width // 2 - 300, 200, 300, 100,
-                             'menu_button.png', 'Новая Игра',
-                             'data/menubtn_sound.mp3', 'menu_button_intersected.png')
-
-    continue_menu_button = Button(width // 2, 200, 300, 100,
-                                  'menu_button.png', 'Продолжить',
+        new_game_button = Button((self.width // 2 - 830, 370), (300, 100),
+                                 'menu_button.png', 'Новая Игра',
+                                 'data/menubtn_sound.mp3', 'menu_button_intersected.png')
+        continue_menu_button = Button((self.width // 2 - 820, 450), (300, 100),
+                                      'menu_button.png', 'Продолжить',
+                                      'data/menubtn_sound.mp3', 'menu_button_intersected.png')
+        settings_menu_button = Button((self.width // 2 - 840, 530), (300, 100),
+                                      'menu_button.png', 'Настройки',
+                                      'data/menubtn_sound.mp3', 'menu_button_intersected.png')
+        exit_menu_button = Button((self.width // 2 - 895, 610), (300, 100),
+                                  'menu_button.png', 'Выход',
                                   'data/menubtn_sound.mp3', 'menu_button_intersected.png')
 
-    settings_menu_button = Button(width // 2 - 150, 300, 300, 100,
-                                  'menu_button.png', 'Настройки',
-                                  'data/menubtn_sound.mp3', 'menu_button_intersected.png')
+        buttons = [new_game_button, continue_menu_button, settings_menu_button, exit_menu_button]
 
-    exit_menu_button = Button(width // 2 - 150, 400, 300, 100,
-                              'menu_button.png', 'Выход',
-                              'data/menubtn_sound.mp3', 'menu_button_intersected.png')
+        while running:
+            self.screen.fill((0, 0, 0))
+            image = pygame.transform.scale(load_image('manekens.png'), (1000, 1000))
+            rect = image.get_rect(topleft=(470, 0))
+            self.screen.blit(image, rect)
+            self.fill_background()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
 
-    buttons = [new_game_button, continue_menu_button, settings_menu_button, exit_menu_button]
+                if event.type == pygame.USEREVENT and event.button == new_game_button:
+                    new_game_menu()
 
-    while running:
-        screen.fill((0, 0, 0))
-        fill_background()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+                if event.type == pygame.USEREVENT and event.button == continue_menu_button:
+                    pass
 
-            if event.type == pygame.USEREVENT and event.button == new_game_button:
-                pass
+                if event.type == pygame.USEREVENT and event.button == settings_menu_button:
+                    self.settings_menu()
 
-            if event.type == pygame.USEREVENT and event.button == continue_menu_button:
-                pass
+                if event.type == pygame.USEREVENT and event.button == exit_menu_button:
+                    running = False
 
-            if event.type == pygame.USEREVENT and event.button == settings_menu_button:
-                pass
+                for button in buttons:
+                    button.button_pressed(event)
 
-            if event.type == pygame.USEREVENT and event.button == exit_menu_button:
-                running = False
+            font = pygame.font.Font('data/MorfinSans-Regular.ttf', 150)
+            text_rendered = font.render('Source', 1, (255, 0, 0))
+            text_rect = text_rendered.get_rect(center=(300, 100))
+            self.screen.blit(text_rendered, text_rect)
+
+            text_rendered = font.render('of', 1, (255, 0, 0))
+            text_rect = text_rendered.get_rect(center=(160, 200))
+            self.screen.blit(text_rendered, text_rect)
+
+            text_rendered = font.render('Fear', 1, (255, 0, 0))
+            text_rect = text_rendered.get_rect(center=(210, 300))
+            self.screen.blit(text_rendered, text_rect)
 
             for button in buttons:
-                button.button_pressed(event)
+                button.draw(self.screen, pygame.mouse.get_pos())
 
-        font = pygame.font.Font('data/plasma-drip-brk.ttf', 85)
-        text_rendered = font.render('Source of Fear', 1, (110, 200, 110))
-        text_rect = text_rendered.get_rect(center=(350, 100))
-        screen.blit(text_rendered, text_rect)
+            pygame.display.flip()
+            self.clock.tick(60)
+        pygame.quit()
 
-        for button in buttons:
-            button.draw(screen, pygame.mouse.get_pos())
+    def settings_menu(self):
+        running = True
 
-        pygame.display.flip()
-        clock.tick(60)
-    pygame.quit()
+        back_button = Button((self.width // 2 - 150, 600), (300, 100),
+                             'menu_button.png', 'Назад',
+                             'data/menubtn_sound.mp3', 'menu_button_intersected.png')
+
+        buttons = [back_button]
+
+        while running:
+            self.screen.fill((0, 0, 0))
+            self.fill_background()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+                if ((event.type == pygame.USEREVENT and event.button == back_button) or
+                        (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE)):
+                    self.main_window()
+
+                for button in buttons:
+                    button.button_pressed(event)
+
+            font1 = pygame.font.Font('data/MorfinSans-Regular.ttf', 150)
+            text_rendered = font1.render('Настройки', 1, (255, 0, 0))
+            text_rect = text_rendered.get_rect(center=((self.width // 2 - text_rendered.get_width() // 2), 90))
+            self.screen.blit(text_rendered, text_rect)
+
+            font2 = pygame.font.Font('data/MorfinSans-Regular.ttf', 60)
+            text_rendered = font2.render('Громкость', 1, (255, 255, 255))
+            text_rect = text_rendered.get_rect(center=((self.width // 6), 270))
+            self.screen.blit(text_rendered, text_rect)
+
+            text_rendered = font2.render('Яркость', 1, (255, 255, 255))
+            text_rect = text_rendered.get_rect(center=((self.width // 6), 390))
+            self.screen.blit(text_rendered, text_rect)
+
+            text_rendered = font2.render('Музыка', 1, (255, 255, 255))
+            text_rect = text_rendered.get_rect(center=((self.width // 6), 510))
+            self.screen.blit(text_rendered, text_rect)
+
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_pressed = pygame.mouse.get_pressed()[0]
+
+            for button in buttons:
+                button.draw(self.screen, mouse_pos)
+
+            sliders = [Slider((720, 265), (450, 35), 0.3, 0, 100),
+                       Slider((720, 385), (450, 35), 0.7, 0, 10)]
+
+            for slider in sliders:
+                if slider.slider_rect.collidepoint(mouse_pos) and mouse_pressed:
+                    slider.move(mouse_pos)
+                slider.render(self.screen)
+
+            pygame.display.flip()
+            self.clock.tick(60)
+        pygame.quit()
 
 
 def new_game_menu():
@@ -136,13 +206,67 @@ def continue_game_menu():
     pass
 
 
-def settings_menu():
-    pass
-
-
 def exit_menu():
     pass
 
 
+class Office:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1920, 1080), FULLSCREEN)
+        pygame.mixer.music.load('data/vent.mp3')
+        pygame.mixer.music.set_volume(0.8)
+        pygame.mixer.music.play(-1)
+        mainloop = True
+        while mainloop:
+            bg = load_image('office.png').convert()
+            bg = pygame.transform.scale(bg, (1920, 1080))
+            for event in pygame.event.get():
+                self.screen.blit(bg, (0, 0))
+                if event.type == pygame.QUIT:
+                    mainloop = False
+                self.time()
+            pygame.display.update()
+
+    def time(self):
+        font = pygame.font.Font('data/plasma-drip-brk.ttf', 85)
+        text_rendered = font.render('Source of Fear', 1, (110, 200, 110))
+        text_rect = text_rendered.get_rect(center=(350, 100))
+        self.screen.blit(text_rendered, text_rect)
+
+    pygame.quit()
+
+
+class Slider:
+    def __init__(self, pos: tuple, size: tuple, start_value: float, min_value: int, max_value: int):
+        self.pos = pos
+        self.size = size
+
+        self.slider_left = self.pos[0] - size[0] // 2
+        self.slider_right = self.pos[0] + size[0] // 2
+        self.slider_top = self.pos[1] - size[1] // 2
+
+        self.min = min_value
+        self.max = max_value
+
+        self.start_value = (self.slider_right - self.slider_left) * start_value
+
+        self.slider_rect = pygame.Rect(self.slider_left, self.slider_top, self.size[0], self.size[1])
+        self.button_rect = pygame.Rect(self.slider_left + self.start_value, self.slider_top + 5, 10, self.size[1] - 10)
+
+    def render(self, sc):
+        pygame.draw.rect(sc, pygame.Color('white'), self.slider_rect, width=5)
+        pygame.draw.rect(sc, pygame.Color('red'), self.button_rect)
+
+    def move(self, mouse_pos):
+        self.button_rect.centerx = mouse_pos[0]
+
+    def get_value(self):
+        value_range = self.slider_right - self.slider_left - 1
+        button_value = self.button_rect.centerx - self.slider_left
+
+        return (button_value / value_range) * (self.max - self.min) + self.min
+
+
 if __name__ == '__main__':
-    main_menu()
+    MainMenu().main_window()
